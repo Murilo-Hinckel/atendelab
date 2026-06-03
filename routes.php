97f80602353efc7582bda8b3912cdaed59b79894
MURILO OTÁVIO DE OLIVEIRA HINCKEL <murilo.hinckel@univille.br>
2026-06-03 20:39:22 -0300

    Metodos

---
+++ routes.php
@@ -0,0 +1,37 @@
+<?php
+require_once __DIR__ . 'app/Controllers/UsuarioController.php';
+
+$_controller = $_GET['controller'] ?? 'home';
+$_action = $_GET['action'] ?? 'index';
+
+if(
+    $_controller === 'usuario' {
+        UsuarioController = new UsuarioController();
+
+        switch ($_action) {
+            case 'listar':
+                $usuarioController->listar();
+                break;
+            
+            case 'buscar':
+                $usuarioController->buscarPorId();
+                break;
+
+            case 'criar':
+                $usuarioController->criar();
+                break;
+            
+            case 'atualizar':
+                $usuarioController->atualizar();
+                break;
+
+            case 'excluir':
+                $usuarioController->excluir();
+                break;
+
+            defoult:
+                echo "AAAAAAAAAAAAAAAAAAAAAAAAAA__404__AAAAAAAAAAAAAAAAAAAAAAAAAA.";
+                break;
+        }
+    }
+)
